530b446032e25fdf767a61a93865bb85cfdb2290
planted-sam <111303389+planted-sam@users.noreply.github.com>
2025-01-02 18:23:09 +0000

    update comment

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const polygonOptions = { color: "purple" };
 
 function App() {
   const [file, setFile] = useState<null | { name: string; data: ArrayBuffer }>(
-    null,
+    null
   );
 
   const [polygons, setPolygons] = useState<[number, number][][]>([]);
@@ -19,12 +19,12 @@ function App() {
       data: ArrayBuffer;
     };
     setFile(newFile);
-    const { polygons: newPolygons, _errors } = await kmlOrKmzToPolygons(
+    const { polygons: newPolygons } = await kmlOrKmzToPolygons(
       newFile.name,
-      newFile.data,
+      newFile.data
     );
     const parsedPolygons = newPolygons.map((polygon) =>
-      polygon.points.map((point) => [point[1], point[0]]),
+      polygon.points.map((point) => [point[1], point[0]])
     ) as [number, number][][];
     setPolygons(parsedPolygons);
   };
@@ -38,15 +38,15 @@ function App() {
       <div className="card">
         <p>{file !== null ? file.name : "no file uploaded"}</p>
       </div>
-
+      {/** Note that leaflet expects coords in lat/lon order */}
       <MapContainer
         className="map"
         center={[32.5939129980001, -92.44634703]}
         zoom={20}
       >
         <TileLayer
-          attribution="MapTiler"
-          url="https://api.maptiler.com/tiles/satellite-v2/{z}/{x}/{y}.jpg?key=8hSCSEUoL6Ko5Bmh1N68"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Polyline pathOptions={polygonOptions} positions={polygons} />
       </MapContainer>
